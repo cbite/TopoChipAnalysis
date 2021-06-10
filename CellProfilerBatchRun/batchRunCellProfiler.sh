981b17ea898d045b175e2c49aff4cc5f2c0f4afe
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2021-06-10 15:21:26 +0200

    Update batchRunCellProfiler.sh
    Update

--- CellProfilerBatchRun/batchRunCellProfiler.sh
+++ CellProfilerBatchRun/batchRunCellProfiler.sh
@@ -3,9 +3,9 @@
 echo "Start Bash Run CellProfiler"
 
 # Set the directory of the batchfile (.hd5 format) and the output folder
-DIRECOTRY_BATCHFILE="C:/Users/tkuijpe1/Desktop/tmp/Batch_data.h5"
-DIRECTORY_OUTPUTDATA="C:/Users/tkuijpe1/Desktop/tmp/OutputData/"
-DIRECTORY_CELLPROFILER="C:/Program Files/CellProfiler/CellProfiler.exe"
+DIRECOTRY_BATCHFILE=""
+DIRECTORY_OUTPUTDATA=""
+DIRECTORY_CELLPROFILER=""
 
 for i in $(seq 1 545 4356); do
 	BATCH_START=$i
